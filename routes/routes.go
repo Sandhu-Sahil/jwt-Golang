237c62f2772b9d87989819e90f010e3d385b40bc
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-02-24 12:24:52 +0530

    Created routes extended struct

--- routes/routes.go
+++ routes/routes.go
@@ -17,18 +17,6 @@ func NewRouterService(usercontroller controllers.UserController) RouterService {
 	}
 }
 
-// func (uc *ExtendedUserController) RegisterLoginRoutes(rg *gin.RouterGroup) {
-// 	userroute := rg.Group("/user")
-// 	userroute.POST("/login", uc.Login)
-// 	userroute.POST("/register", uc.Register)
-// }
-
-// func (uc *ExtendedUserController) RegisterJwtCheckRoutes(rg *gin.RouterGroup) {
-// 	userroute := rg.Group("/check")
-// 	userroute.Use(middlewares.JwtAuthMiddleware())
-// 	userroute.GET("/get/:id", uc.GetUser)
-// }
-
 func (rs *RouterService) RegisterLoginRoutes(rg *gin.RouterGroup) {
 	userroute := rg.Group("/user")
 	userroute.POST("/login", rs.Usercontroller.Login)
